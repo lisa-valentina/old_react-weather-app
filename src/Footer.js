6d4d350a60d6847d3f72ed2326f2a4595e2ccbcc
lisa-valentina <lisa.riedel@gmail.com>
2023-08-19 12:19:10 +0200

    Updated footer

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <a
+        href="https://github.com/lisa-valentina/react-weather-app.git"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code
+      </a>{" "}
+      by Lisa Valentina Riedel.
+    </div>
+  );
+}
