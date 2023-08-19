bea5436a84d02d761eb00a6ae5e934f7eeb61627
lisa-valentina <lisa.riedel@gmail.com>
2023-08-19 13:18:22 +0200

    included GitHub Link

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,15 @@
 export default function Footer() {
   return (
     <div>
+      This app was coded by Lisa Valentina Riedel and is{" "}
       <a
         href="https://github.com/lisa-valentina/react-weather-app.git"
         target="_blank"
         rel="noreferrer"
       >
-        Open-source code
-      </a>{" "}
-      by Lisa Valentina Riedel.
+        open-sourced on GitHub
+      </a>
+      .
     </div>
   );
 }
